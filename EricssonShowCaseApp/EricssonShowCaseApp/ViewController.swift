2d4c158457b59b4be9e1685b591eddf5c60fdfb0
mohamedkleit <moh.kleit@gmail.com>
2016-07-13 09:22:15 -0400

    Login page and register page (night not be necessary but we can delete it after)

---
+++ EricssonShowCaseApp/EricssonShowCaseApp/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  EricssonShowCaseApp
+//
+//  Created by Mohamed Kleit on 2016-07-12.
+//  Copyright © 2016 Mohamed Kleit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool){
+        self.performSegueWithIdentifier("loginView", sender: self);
+    }
+
+
+}
+
